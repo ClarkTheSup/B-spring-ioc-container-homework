373952e1f1e83a0de135dd0232fc0c2a3ecec5ea
ClarkTheSup <lknkoqq123@163.com>
2020-09-17 15:37:02 +0800

    [kangning] feat: ready for showcase

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,9 +1,5 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
     @Autowired
-    private GreetingControllerAspect greetingControllerAspect;
+    GreetingService greetingService;
 
     @GetMapping("/greet")
     public String greet() {
-//        this.greetingService = new GreetingService();
-        GreetingService greetingService = greetingControllerAspect.getGreetingService();
         return greetingService.sayHi();
     }
 
